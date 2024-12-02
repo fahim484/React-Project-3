53c2686154e80858dcc109d59454e81787893862
fahim484 <fahim.faysal.webdeveloper@gmail.com>
2024-12-02 16:02:57 +0600

    next ui added successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import HeroArea from "./components/HeroArea";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <HeroArea />
     </>
   );
 }
