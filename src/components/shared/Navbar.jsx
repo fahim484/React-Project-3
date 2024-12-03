7a76d34ae82c38837f25c6a2db2329ee2686cf23
fahim484 <fahim.faysal.webdeveloper@gmail.com>
2024-12-03 17:02:34 +0600

    header-navbar section done

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import {
   Navbar,
-  NavbarBrand,
+  NavbarContent,
   NavbarMenuToggle,
+  NavbarBrand,
   NavbarMenuItem,
   NavbarMenu,
-  NavbarContent,
   NavbarItem,
   Link,
+  Badge,
 } from "@nextui-org/react";
 
 import {
@@ -17,8 +19,148 @@ import {
   Button,
 } from "@nextui-org/react";
 
+import { ChevronDownIcon, UserIcon } from "@heroicons/react/24/outline";
+
 function HeaderNavbar() {
-  return <div>Navbar</div>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuItems = [
+    "Eat & Drink",
+    "Club",
+    "Things to do",
+    "Shopping",
+    "Account",
+    "Contact",
+  ];
+
+  return (
+    <section>
+      <Navbar
+        isMenuOpen={isMenuOpen}
+        onMenuOpenChange={setIsMenuOpen}
+        className="w-full"
+      >
+        <NavbarContent className="sm:hidden" justify="start">
+          <NavbarMenuToggle
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          ></NavbarMenuToggle>
+        </NavbarContent>
+
+        <NavbarContent className="sm:hidden pr-3">
+          <NavbarBrand>
+            <img className="w-44" src="/Img/main-logo.png" alt="Main Logo" />
+          </NavbarBrand>
+        </NavbarContent>
+
+        {/* Desktop Device  */}
+
+        <NavbarContent className="hidden sm:flex gap-4 w-full justify-center text-[]">
+          <NavbarBrand>
+            <img className="w-44" src="/Img/main-logo.png" alt="Main Logo" />
+          </NavbarBrand>
+
+          <NavbarItem>
+            <Link className="text-[#26395C]">Eat & drink</Link>
+          </NavbarItem>
+
+          <NavbarItem>
+            <Link className="text-[#26395C]">
+              Club <sup className="w-[41.95px] h-[16.5px] bg-[#0E8BFF] text-[#FFFFFF] text-[8px] rounded-[23px] flex items-center justify-center mb-5">⭐ HOT</sup>
+            </Link>
+          </NavbarItem>
+
+          <NavbarItem>
+            <Dropdown>
+              <DropdownTrigger>
+                <Button variant="none">
+                  Things to do
+                  <ChevronDownIcon className="text-[#0E8BFF] w-4 h-4 font-bold" />
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu
+                aria-label="Example with disabled actions"
+                disabledKeys={["edit", "delete"]}
+              >
+                <DropdownItem key="new">New file</DropdownItem>
+                <DropdownItem key="copy">Copy link</DropdownItem>
+                <DropdownItem key="edit">Edit file</DropdownItem>
+                <DropdownItem
+                  key="delete"
+                  className="text-danger"
+                  color="danger"
+                >
+                  Delete file
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </NavbarItem>
+
+          <NavbarItem>
+            <Link className="foreground">
+              <Badge
+                color="primary"
+                content={3}
+                shape="circle"
+                placement="top-left"
+                size="md"
+              >
+                <img src="Img/navbar-logos/shopping-cart.png" alt="" />
+              </Badge>
+            </Link>
+          </NavbarItem>
+
+          <NavbarItem>
+            <Dropdown>
+              <DropdownTrigger>
+                <Button variant="none">
+                  <UserIcon className="text-[#26395C] w-4 h-4" /> Account
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu aria-label="profile">
+                <DropdownItem key="new">New file</DropdownItem>
+                <DropdownItem key="copy">Copy link</DropdownItem>
+                <DropdownItem key="edit">Edit file</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </NavbarItem>
+
+          <NavbarItem>
+            <Button color="primary">Contact Now</Button>
+          </NavbarItem>
+
+        </NavbarContent>
+
+        <div className="lg:hidden">
+        <NavbarContent className="w-full" justify="end">
+          <NavbarItem className="hidden lg:flex"> </NavbarItem>
+
+          <NavbarItem>
+          <Badge
+                color="primary"
+                content={3}
+                shape="circle"
+                placement="top-left"
+                size="md"
+              >
+                <img src="Img/navbar-logos/shopping-cart.png" alt="" />
+              </Badge>
+          </NavbarItem>
+        </NavbarContent>
+
+        <NavbarMenu className="bg-[#F5FAFF]">
+          {menuItems.map((item, index) => (
+            <NavbarMenuItem key={index}>
+              <Link className="w-full text-[#26395C] capitalize ">{item}</Link>
+            </NavbarMenuItem>
+          ))}
+        </NavbarMenu>
+        </div>
+
+
+
+      </Navbar>
+    </section>
+  );
 }
 
 export default HeaderNavbar;
